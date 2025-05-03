e845a869895cb80af2bceca0912b99fdd01de40c
John Mintz <john.mintz@gmail.com>
2025-05-03 07:51:23 -0500

    Printing Numbers

---
+++ Print Numbers.py
@@ -0,0 +1,15 @@
+#Practicing printing in python
+#John W. Mintz
+#john.mintz@gmail.com
+
+print ("1")
+print ("2")
+print ("3")
+print ("4")
+print ("5")
+print ("6")
+print ("7")
+print ("8")
+print ("9")
+print ("10")
+print ("11")
