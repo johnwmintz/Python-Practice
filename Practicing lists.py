c9ce8f2be19b3f4863b078299ed374b66ae3e90a
John Mintz <john.mintz@gmail.com>
2025-07-15 20:12:00 -0500

    Lists

---
+++ Practicing lists.py
@@ -0,0 +1,20 @@
+#Practicing lists
+#John W. Mintz
+#john.mintz@gmail.com
+
+
+#This is the working list
+L= [1,2,3,4,5]
+
+print ("This is a normal list")
+print (L)
+
+#This is working work lists on reverse and apppend
+
+L.append(9)
+L.append(25)
+L.reverse()
+
+print("This is a reverse of working list while addding 9 and 25")
+print(L)
+
