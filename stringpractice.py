7f67c65533b23b615897091296a72a0cd9c668c7
John Mintz <john.mintz@gmail.com>
2025-07-15 19:32:07 -0500

    Adding to github

---
+++ stringpractice.py
@@ -0,0 +1,14 @@
+#Practicing strings
+#John W. Mintz
+#john.mintz@gmail.com
+
+
+PackingList='ruck sack', 'weapons cleaning kit', 'wet weather gear' ,'woobie'
+
+#print (PackingList)
+
+PackingList.find ('woobie')
+
+print (PackingList.find)
+
+#still working this
