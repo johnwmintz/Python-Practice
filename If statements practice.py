5828a63891edae3cf77ff575f9cebda8e3e47cdd
John Mintz <john.mintz@gmail.com>
2025-07-17 15:44:25 -0500

    If Statements

---
+++ If statements practice.py
@@ -0,0 +1,15 @@
+#If statements practice
+#John W. Mintz
+#john.mintz@gmail.com
+
+import math
+
+x = 9
+y = 4
+
+if x > y:
+
+
+    print ("x is greater than y")
+else:
+    print ("y is greater than x")
